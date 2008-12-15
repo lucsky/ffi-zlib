ab53ce0aaf1c25e44c0c7b1ef9645b29ed0afd29
Luc Heinrich <luc@honk-honk.com>
2008-12-15 15:30:37 +0100

    Added test case for the 'misc' functions.

---
+++ tests/test_misc.rb
@@ -0,0 +1,38 @@
+require "test/unit"
+require "ffi/zlib"
+require "tests/helper.rb"
+
+class TestMisc < Test::Unit::TestCase
+    
+    def test_version
+        assert_not_nil FFI::Zlib.zlibVersion
+    end
+    
+    def test_compile_flags
+        assert_not_equal FFI::Zlib.zlibCompileFlags, 0
+    end
+    
+    def test_crc_table
+        table = FFI::Zlib.get_crc_table
+        assert_not_nil table
+    end
+    
+    def test_errors
+        do_test_error FFI::Zlib::Z_OK
+        do_test_error FFI::Zlib::Z_STREAM_END
+        do_test_error FFI::Zlib::Z_NEED_DICT
+        do_test_error FFI::Zlib::Z_ERRNO
+        do_test_error FFI::Zlib::Z_STREAM_ERROR
+        do_test_error FFI::Zlib::Z_DATA_ERROR
+        do_test_error FFI::Zlib::Z_MEM_ERROR
+        do_test_error FFI::Zlib::Z_BUF_ERROR
+        do_test_error FFI::Zlib::Z_VERSION_ERROR
+    end
+    
+    def do_test_error(err)
+        err_str = FFI::Zlib.zError(err)
+        assert_not_nil err_str
+        assert_instance_of String, err_str
+    end
+    
+end
